287f0d0128d4cf659c4da1d5ec2d9c0cccefcb7c
mamijose <mmammyj@gmail.com>
2023-07-31 10:24:28 +0200

    the other commit

--- Exercise 2 - Word Cloud/cloud.js
+++ Exercise 2 - Word Cloud/cloud.js
@@ -1,1 +1,22 @@
 console.log("Exercise 2 - Word Cloud");
+
+let myParagraph = document.querySelector("#myParagraph")
+let paraText = myParagrapharagraph.textContent.toLowerCase().split(" ")
+let Object = {}
+let cloud = document.getElementById("myWordCloud")
+
+
+for(let i = 0; i < paraText.length; i++){
+    if(Object.hasOwnProperty(paraText[i])){
+        paraObject[paraText[i]]++
+    }
+    else{
+        Object[paraText[i]] = 1
+    }
+}
+
+let sortedPara = Object.fromEntries(Object.entries(paraObject).sort(([,a], [,b]) => b - a))
+let twelveCommon = Object.keys(Object.fromEntries(Object.entries(sortedPara).slice(1,13)))
+
+
+
